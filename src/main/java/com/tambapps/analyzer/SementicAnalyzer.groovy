fca1e4c097fa8fa9d8b267c72f7997c1449d6fb2
headhunteerr <nicolas.mandadjiev@gmail.com>
2018-09-12 11:28:30 +0200

    SementicAnalyzer and DequeMap

---
+++ src/main/java/com/tambapps/analyzer/SementicAnalyzer.groovy
@@ -0,0 +1,11 @@
+package com.tambapps.analyzer
+
+import com.tambapps.util.DequeMap
+
+import java.util.concurrent.LinkedBlockingDeque;
+
+class SementicAnalyzer {
+
+    DequeMap dqMap = new DequeMap()
+
+}
