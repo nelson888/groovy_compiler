8baf3825fff1f7d79f75b18fe590bfd64322806c
Fonkoua Tambue Nelson <nfonkoua@weborama.com>
2018-12-18 11:52:31 +0100

    created evaluator class

---
+++ src/main/groovy/com/tambapps/compiler/eval/Evaluator.groovy
@@ -0,0 +1,100 @@
+package com.tambapps.compiler.eval
+
+import com.tambapps.compiler.analyzer.token.TokenNode
+import com.tambapps.compiler.analyzer.token.TokenNodeType
+import com.tambapps.compiler.exception.SemanticException
+import com.tambapps.compiler.exception.SymbolException
+import com.tambapps.compiler.util.DequeMap
+import com.tambapps.compiler.util.Symbol
+
+class Evaluator {
+
+  private DequeMap dequeMap = new DequeMap()
+  private int nbSlot = 0
+
+  void process(TokenNode node) throws SemanticException {
+    switch (node.type) {
+      case TokenNodeType.VAR_DECL:
+        Symbol s
+        try {
+          s = dequeMap.newSymbol(node.value)
+        } catch (SymbolException e) {
+          throw new SemanticException(e.message, node.l, node.c)
+        }
+        s.slot = nbSlot++
+        break
+
+      case TokenNodeType.VAR_REF:
+        Symbol s
+        try {
+          s = dequeMap.findSymbol(node.value.name)
+        } catch (SymbolException e) {
+          throw new SemanticException(e.message, node.l, node.c)
+        }
+
+        node.value.index = s.slot
+        break
+      case TokenNodeType.ASSIGNMENT:
+
+        break
+      case TokenNodeType.D_REF:
+        //TODO
+
+      case TokenNodeType.BLOC:
+        dequeMap.newBlock()
+        for (int i = 0; i < node.nbChildren(); i++) {
+          process(node.getChild(i))
+        }
+        try {
+          dequeMap.endBlock()
+        } catch (SymbolException e) {
+          throw new SemanticException(e.message, node.l, node.c)
+        }
+        break
+
+      case TokenNodeType.FUNCTION:
+        nbSlot = 0
+        Symbol s
+        try {
+          s = dequeMap.newSymbol(node.value.name)
+        } catch (SymbolException e) {
+          throw new SemanticException(e.message, node.l, node.c)
+        }
+        int nbArgs = node.nbChildren() - 1
+        s.nbArgs = nbArgs
+        node.value.nbArgs = nbArgs
+        dequeMap.newBlock()
+        for (int i = 0; i < node.nbChildren(); i++) {
+          process(node.getChild(i))
+        }
+        dequeMap.endBlock()
+        s.slot = nbSlot - s.nbArgs
+        break
+
+      case TokenNodeType.FUNCTION_CALL:
+        Symbol s
+        try {
+          s = dequeMap.findSymbol(node.value.name)
+        } catch (SymbolException e) {
+          throw new SemanticException(e.message, node.l, node.c)
+        }
+        if (!s.function) {
+          throw new SemanticException("Cannot call a variable ($s.ident)", node.l, node.c)
+        }
+        if (s.nbArgs != node.nbChildren()) {
+          throw new SemanticException("Function $s.ident expects $s.nbArgs parameters (got ${node.nbChildren()})",
+              node.l, node.c)
+        }
+        for (int i = 0; i < node.nbChildren(); i++) {
+          process(node.getChild(i))
+        }
+        break
+
+      default:
+        for (int i = 0; i < node.nbChildren(); i++) {
+          process(node.getChild(i))
+        }
+    }
+  }
+
+}
