05766970de1d69fc03694458fc42eb0cb9defe06
Fonkoua Tambue Nelson <nfonkoua@weborama.com>
2018-12-20 10:40:11 +0100

    corrected code optimizer

--- src/main/groovy/com/tambapps/compiler/optimizer/code/CodeOptimizer.groovy
+++ src/main/groovy/com/tambapps/compiler/optimizer/code/CodeOptimizer.groovy
@@ -5,7 +5,7 @@ class CodeOptimizer {
   void optimize(List<String> lines) {
     for (int i = 0; i < lines.size() - 2; i++) {
       def frame = [lines.get(i), lines.get(i + 1), lines.get(i + 2)]
-      if (frame[0].startsWith("push.i") && frame[2].startsWith("drop")) {
+      if (frame[0].startsWith("dup") && frame[2].startsWith("drop")) {
         lines.remove(i + 2)
         lines.remove(i)
         i-= 2
